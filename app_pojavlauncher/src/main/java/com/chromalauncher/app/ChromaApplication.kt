47988aec6db5156d0a1a08ac963e6cf698b1918f
VonKen <vonken@chromalauncher.dev>
2026-07-16 18:39:15 +0000

    Strip manifest to bare minimum - fix instant crash

--- app_pojavlauncher/src/main/java/com/chromalauncher/app/ChromaApplication.kt
+++ app_pojavlauncher/src/main/java/com/chromalauncher/app/ChromaApplication.kt
@@ -1,17 +1,35 @@
 package com.chromalauncher.app
 
+import android.app.Application
+import android.os.Environment
 import android.os.StrictMode
 import android.util.Log
 import net.kdt.pojavlaunch.BuildConfig
-import net.kdt.pojavlaunch.PojavApplication
+import net.kdt.pojavlaunch.Tools
+import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
 
-class ChromaApplication : PojavApplication() {
+class ChromaApplication : Application() {
 
     override fun onCreate() {
+        super.onCreate()
+        instance = this
+
+        Thread.setDefaultUncaughtExceptionHandler { _, th ->
+            try {
+                val crashFile = File(cacheDir, "crash.txt")
+                val sw = StringWriter()
+                th.printStackTrace(PrintWriter(sw))
+                crashFile.writeText("ChromaLauncher Crash\n${sw.toString()}")
+                Log.e("ChromaApplication", "Crash saved to ${crashFile.absolutePath}", th)
+            } catch (ignored: Throwable) {}
+        }
+
         try {
-            super.onCreate()
+            initDirectories()
         } catch (e: Throwable) {
-            Log.e("ChromaApplication", "PojavApplication init failed, continuing with minimal init", e)
+            Log.e("ChromaApplication", "Directory init failed", e)
         }
 
         if (BuildConfig.DEBUG) {
@@ -22,8 +40,27 @@ class ChromaApplication : PojavApplication() {
                     .build()
             )
         }
+    }
 
-        instance = this
+    private fun initDirectories() {
+        val ctx = this
+        Tools.DIR_DATA = ctx.filesDir.parent
+        Tools.DIR_CACHE = ctx.cacheDir
+        val extDir = if (android.os.Build.VERSION.SDK_INT >= 29) {
+            ctx.getExternalFilesDir(null) ?: File(Environment.getExternalStorageDirectory(), "games/ChromaLauncher")
+        } else {
+            File(Environment.getExternalStorageDirectory(), "games/ChromaLauncher")
+        }
+        Tools.DIR_GAME_HOME = extDir.absolutePath
+        Tools.DIR_GAME_NEW = extDir.absolutePath + "/.minecraft"
+        Tools.MULTIRT_HOME = Tools.DIR_DATA + "/runtimes"
+        Tools.DIR_ACCOUNT_NEW = Tools.DIR_DATA + "/accounts"
+        Tools.CTRLMAP_PATH = extDir.absolutePath + "/controlmap"
+        Tools.CTRLDEF_FILE = extDir.absolutePath + "/controlmap/default.json"
+        Tools.NATIVE_LIB_DIR = applicationInfo.nativeLibraryDir
+        Tools.ASSETS_PATH = extDir.absolutePath + "/assets"
+        Tools.OBSOLETE_RESOURCES_PATH = extDir.absolutePath + "/resources"
+        Tools.APP_NAME = "ChromaLauncher"
     }
 
     companion object {
